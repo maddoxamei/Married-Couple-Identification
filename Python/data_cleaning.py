44aa0bb0f9a8836f41652d90eafea025830deb1a
Thomas FitzGerald <thomas.fitzgerald23@ncf.edu>
2022-04-30 12:37:41 -0400

    Updated df names in data_cleaning

--- Python/data_cleaning.py
+++ Python/data_cleaning.py
@@ -14,17 +14,17 @@ for key, df in dfs.items():
 
 #Transactions
 #Replace low NA % categorical with 'unknown'
-dfs.get('transactions')['trans_spending_category'].fillna('unknown',inplace=True)
-dfs.get('transactions')['transaction_type'].fillna('unknown',inplace=True)
+dfs.get('transaction')['trans_spending_category'].fillna('unknown',inplace=True)
+dfs.get('transaction')['transaction_type'].fillna('unknown',inplace=True)
 #Dropping high NA % columns
-dfs.get('transactions').drop(['merch_x_coord'], axis=1, inplace=True) 
-dfs.get('transactions').drop(['merch_y_coord'], axis=1, inplace=True)
+dfs.get('transaction').drop(['merch_x_coord'], axis=1, inplace=True) 
+dfs.get('transaction').drop(['merch_y_coord'], axis=1, inplace=True)
 
 #Customer Demographics
 #I'm not sure why fillna doens't work on this one, but this works.
-median_val = np.nanmedian(dfs.get('customer_demog_1')['customer_income_level'])
-dfs.get('customer_demog_1')['customer_income_level'] = dfs.get('customer_demog_1')['customer_income_level'].replace(np.nan,median_val)
-#dfs.get('customer_demog_1')['customer_income_level'].fillna(median_val, inplace=True) #This doesn't work.
+median_val = np.nanmedian(dfs.get('customer_demog')['customer_income_level'])
+dfs.get('customer_demog')['customer_income_level'] = dfs.get('customer_demog')['customer_income_level'].replace(np.nan,median_val)
+#dfs.get('customer_demog')['customer_income_level'].fillna(median_val, inplace=True) #This doesn't work.
 
 
 
