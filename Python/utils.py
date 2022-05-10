621555c3ab7e09bde67ff6be6135fa4d628b65b3
Amelia Maddox <maddoxamei@gmail.com>
2022-05-09 22:16:25 -0400

    Encoding alterations

--- Python/utils.py
+++ Python/utils.py
@@ -19,3 +19,29 @@ _translation_reference = _schema.groupby('TABLE')[['VARIABLE_NAME','TRANSLATION'
 # Read all CSV's in the directory denoted by <path>
 #	{filename : pandas.DataFrame}
 dfs = dict([(_df_name_reference.get(csv), pd.read_csv( os.path.join(data_path, csv) ).rename(columns = _translation_reference.get(_table_reference.get(csv)))) for csv in os.listdir(data_path) if csv.endswith('.txt')])
+
+
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    """ Create a visualization of a progress bar updates according to completion status
+    :param it: job you are trying to create a progress bar for
+    :type obj (sequence or collection)
+    :param prefix: The text to display to the left of the status bar
+    :type str
+    :param size: total length of the progress bar
+    :type int
+    :param file: what to display/write the progress bar to
+    :type output stream
+    :return: job you are trying to create a progress bar for
+    :rtype: obj (sequence or collection)
+    """
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()        
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()
